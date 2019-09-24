2c1150dff1cfa72c3554f837301204280bf7bb62
João Justo <joaovjusto30@hotmail.com>
2019-09-23 23:54:50 -0300

    First Project Commit

---
+++ SistemaEstacionamento/Views/Principal.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SistemaEstacionamento.Views
+{
+    /// <summary>
+    /// Interaction logic for Principal.xaml
+    /// </summary>
+    public partial class Principal : Window
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente fechar a janela?",
+                "WPF Vendas", MessageBoxButton.YesNo,
+                MessageBoxImage.Question) == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            //frmCadastrarProduto f =
+            //    new frmCadastrarProduto();
+            //f.ShowDialog();
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            //frmCadastrarVenda f =
+            //    new frmCadastrarVenda();
+            //f.ShowDialog();
+        }
+    }
+}
